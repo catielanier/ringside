dd5cb7ef6ed42050a8cf0bc9cf00fa11a992b934
Catie Lanier <hello@catielanier.ca>
2024-05-24 19:43:21 -0400

    create struct for full deck with backstage/backlash/arsenal

--- deck.go
+++ deck.go
@@ -5,25 +5,67 @@ import (
 	"math/rand"
 )
 
-type deck []string
+type alignment int
+type secondaryAlignment int
 
-func newDeck() deck {
-	return deck{"Escape Move", "Step Aside", "Right Cross Punch", "Volley This!", "Reach for the Ropes", "There Are Two Things You Can Do: Nothing and Like It"}
+const (
+	Heel alignment = iota
+	Face
+	Neutral
+	Any
+)
+
+const (
+	Cheater secondaryAlignment = iota
+	FanFavorite
+	SecondaryNeutral
+	SecondaryAny
+)
+
+type superstar = struct {
+	Name               string
+	IsRevolution       bool
+	HandSize           int
+	SuperStarValue     int
+	ArsenalLimit       int
+	BacklashLimit      int
+	BackstageLimit     int
+	DrawSize           int
+	Alignment          alignment
+	SecondaryAlignment secondaryAlignment
+	CardText           string
 }
 
-func (d deck) print() {
-	for i, card := range d {
+type backstageArea []string
+
+type backlashDeck []string
+
+type arsenal []string
+
+type deck struct {
+	Superstar     superstar
+	BackstageArea backstageArea
+	BacklashDeck  backlashDeck
+	Arsenal       arsenal
+}
+
+func newArsenal() arsenal {
+	return arsenal{"Escape Move", "Step Aside", "Right Cross Punch", "Volley This!", "Reach for the Ropes", "There Are Two Things You Can Do: Nothing and Like It"}
+}
+
+func (a arsenal) print() {
+	for i, card := range a {
 		fmt.Println(i, card)
 	}
 }
 
-func (d deck) shuffle() {
-	for i := range d {
+func (a arsenal) shuffle() {
+	for i := range a {
 		j := rand.Intn(i + 1)
-		d[i], d[j] = d[j], d[i]
+		a[i], a[j] = a[j], a[i]
 	}
 }
 
-func deal(d deck, handSize int) (deck, deck) {
-	return d[:handSize], d[handSize:]
+func deal(a arsenal, handSize int) (arsenal, arsenal) {
+	return a[:handSize], a[handSize:]
 }
--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
+import "fmt"
+
 func main() {
-	arsenal := newDeck()
-	arsenal.shuffle()
-	hand, remainingArsenal := deal(arsenal, 2)
-	hand.print()
-	remainingArsenal.print()
+	deck := deck{
+		Superstar: superstar{
+			Name:               "Mankind",
+			IsRevolution:       false,
+			HandSize:           2,
+			SuperStarValue:     4,
+			ArsenalLimit:       60,
+			BacklashLimit:      20,
+			BackstageLimit:     99,
+			DrawSize:           2,
+			Alignment:          Any,
+			SecondaryAlignment: SecondaryAny,
+			CardText:           "You must always draw 2 cards, if possible, during your draw segment. All damage from opponent is at -1D",
+		},
+		BackstageArea: backstageArea{},
+		BacklashDeck:  backlashDeck{},
+		Arsenal:       newArsenal(),
+	}
+	deck.Arsenal.shuffle()
+	hand, remainingArsenal := deal(deck.Arsenal, deck.Superstar.HandSize)
+
+	fmt.Println("Your superstar is: " + deck.Superstar.Name)
+	fmt.Println("Your hand is:")
+	for _, card := range hand {
+		fmt.Println(card)
+	}
+	fmt.Println("Your remaining arsenal is:")
+	for _, card := range remainingArsenal {
+		fmt.Println(card)
+	}
 }
