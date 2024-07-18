aaaf4b6983ed31ca27c8867e8ec1a8c4948b74e7
Catie Lanier <hello@catielanier.ca>
2024-07-18 14:31:20 -0400

    add interfaces for rest of cards

--- src/types/cards.ts
+++ src/types/cards.ts
@@ -1,5 +1,14 @@
-import {ObjectID} from "mongodb";
-import {Brand, Alignment, SecondaryAlignment} from "../utils/constants";
+import { ObjectID } from "mongodb";
+import {
+    BRAND,
+    ALIGNMENT,
+    SECONDARY_ALIGNMENT,
+    ARSENAL_CARD_TYPE,
+    REVERSAL_TYPE,
+    MANEUVER_TYPE,
+    MANEUVER_SUB_TYPE,
+    PRE_MATCH_TYPE
+} from "../utils/constants";
 
 export interface Superstar {
     _id: ObjectID;
@@ -12,7 +21,7 @@ export interface Superstar {
     isNXT: boolean;
     isNWO: boolean;
     isTagTeam: boolean;
-    brand: Brand;
+    brand: typeof BRAND[number];
     isGM: boolean;
     handSize: number;
     superStarValue: number;
@@ -20,8 +29,8 @@ export interface Superstar {
     backlashLimit: number;
     backstageLimit: number;
     drawSize: number;
-    alignment: Alignment;
-    secondaryAlignment: SecondaryAlignment;
+    alignment: typeof ALIGNMENT[number];
+    secondaryAlignment: typeof SECONDARY_ALIGNMENT[number];
     eugeneMode: boolean;
     canAlsoPackSuperstarSpecifics: ObjectID[];
     canBeBackstage: boolean;
@@ -30,4 +39,117 @@ export interface Superstar {
     set: string[];
     cardText: string;
     imageURL: string[];
+}
+
+export interface ArsenalCard {
+    id: ObjectID;
+    title: string;
+    isRevolution: boolean;
+    isThrowback: boolean;
+    isSurvivorSeries: boolean;
+    isForeignObject: boolean;
+    isChain: boolean;
+    isHeat: boolean;
+    isRMS: boolean;
+    isUnique: boolean;
+    isActive: boolean;
+    isBash: boolean;
+    isSetUp: boolean;
+    isRunIn: boolean;
+    isDivaOnly: boolean;
+    isFemaleOnly: boolean;
+    isNXTLogo: boolean;
+    isNWOLogo: boolean;
+    isLegendOnly: boolean;
+    isTagTeamOnly: boolean;
+    brand: typeof BRAND[number];
+    hideCardsUnderneathValue: number;
+    alignment: typeof ALIGNMENT[number];
+    secondaryAlignment: typeof SECONDARY_ALIGNMENT[number];
+    superstarsSpecificTo: ObjectID[];
+    volley: number;
+    stunValue: number;
+    cardType: typeof ARSENAL_CARD_TYPE[number][];
+    isMultiAction: boolean;
+    isMultiAntic: boolean;
+    isHybridMultiActionAntic: boolean;
+    maneuverCardType: typeof MANEUVER_TYPE[number][];
+    maneuverSubType: typeof MANEUVER_SUB_TYPE[number];
+    reversalType: typeof REVERSAL_TYPE[number][];
+    fortitude: number;
+    damage: number;
+    cardText: string;
+    quote: string;
+    set: string[];
+    imageURL: string[];
+}
+
+export interface Backlash {
+    id: ObjectID;
+    title: string;
+    isPreMatch: boolean;
+    isMidMatch: boolean;
+    isThrowback: boolean;
+    isSurvivorSeries: boolean;
+    isForeignObject: boolean;
+    isChain: boolean;
+    isHeat: boolean;
+    isRMS: boolean;
+    isUnique: boolean;
+    isActive: boolean;
+    isBash: boolean;
+    isSetUp: boolean;
+    isRunIn: boolean;
+    isDivaOnly: boolean;
+    isFemaleOnly: boolean;
+    isNxtLogo: boolean;
+    isNwoLogo: boolean;
+    isLegendOnly: boolean;
+    isTagTeamOnly: boolean;
+    brand: typeof BRAND[number];
+    alignment: typeof ALIGNMENT[number];
+    secondaryAlignment: typeof SECONDARY_ALIGNMENT[number];
+    superstarsSpecificTo: ObjectID[];
+    volley: number;
+    stunValue: number;
+    preMatchType: typeof PRE_MATCH_TYPE[number];
+    midMatchType: typeof ARSENAL_CARD_TYPE[number][];
+    isMultiAction: boolean;
+    hideCardsUnderneathValue: number;
+    isMultiAntic: boolean;
+    isHybridMultiActionAntic: boolean;
+    maneuverCardType: typeof MANEUVER_TYPE[number][];
+    maneuverSubType: typeof MANEUVER_SUB_TYPE[number];
+    reversalType: typeof REVERSAL_TYPE[number][];
+    fortitude: number;
+    damage: number;
+    set: string[];
+    cardText: string;
+    quote: string;
+    imageUrl: string[];
+}
+
+interface Enforcer {
+    name: string;
+    handSize: number;
+    superStarValue: number;
+    drawSize: number;
+    cardText: string;
+}
+
+export interface Backstage {
+    id: ObjectID;
+    title: string;
+    isEnforcer: boolean;
+    isObject: boolean;
+    isHideable: boolean;
+    brand: typeof BRAND[number];
+    enforcer: Enforcer[];
+    alignment: typeof ALIGNMENT[number];
+    secondaryAlignment: typeof SECONDARY_ALIGNMENT[number];
+    superstarsSpecificTo: ObjectID[];
+    hideCardsUnderneathValue: number;
+    set: string[];
+    cardText: string;
+    imageUrl: string[];
 }
---
+++ src/utils/constants.ts
@@ -0,0 +1,74 @@
+export const BRAND: readonly string[] = Object.freeze<string[]>([
+    'Raw',
+    'Smackdown',
+    'Neutral'
+])
+
+export const ALIGNMENT: readonly string[] = Object.freeze<string[]>([
+    "Heel",
+    "Face",
+    "Neutral",
+    "Any",
+    "Both"
+])
+
+export const SECONDARY_ALIGNMENT: readonly string[] = Object.freeze<string[]>([
+    'Cheater',
+    'Fan Favorite',
+    'Neutral',
+    'Any',
+    'Both'
+])
+
+export const ARSENAL_CARD_TYPE: readonly string[] = Object.freeze<string[]>([
+    'Maneuver',
+    'Action',
+    'Antic',
+    'Reversal'
+])
+
+export const REVERSAL_TYPE: readonly string[] = Object.freeze<string[]>([
+    'Grapple',
+    'Strike',
+    'Submission',
+    'Action',
+    'High Risk',
+    'Assault',
+    'Hold',
+    'Throw',
+    'Extreme',
+    'Antic',
+    'Special'
+])
+
+export const MANEUVER_TYPE: readonly string[] = Object.freeze<string[]>([
+    'Strike',
+    'Grapple',
+    'Submission',
+    'High Risk',
+    'Assault',
+    'Hold',
+    'Throw',
+    "Extreme",
+    'Trademark Finisher',
+    'Trademark',
+    'Non-Maneuver'
+])
+
+export const MANEUVER_SUB_TYPE: readonly string[] = Object.freeze<string[]>([
+    'Core',
+    'Mean',
+    'Colossal',
+    'Non-Revolution'
+])
+
+export const PRE_MATCH_TYPE: readonly string[] = Object.freeze<string[]>([
+    'Venue',
+    'Feud',
+    'Stipulation',
+    'Event',
+    'Object',
+    'Pay-Per-View Event',
+    'Manager',
+    'Non-Pre-Match'
+])
